fd25c0fcdacbcc5618e5163fedf45854563e1cd9
Willileo <97127192+Willileo24@users.noreply.github.com>
2023-03-13 19:54:51 +0100

    Aufgaben von Seite 33 im Buch

---
+++ 06Seite33/Planet.java
@@ -0,0 +1,16 @@
+import greenfoot.*; // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class Planet extends World
+{
+    public Planet()
+    {
+        super(16, 12, 50);
+
+        this.setPaintOrder(String.class, Rover.class, Marke.class, Gestein.class, Huegel.class);
+        Greenfoot.setSpeed(20); 
+        
+        
+        this.addObject(new Rover(), 1, 1);
+        this.addObject(new Gestein(), 1, 5);
+    }  
+}
